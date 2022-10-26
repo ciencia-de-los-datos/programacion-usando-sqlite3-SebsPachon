bfffb218fb9f2f19d42398c7e326b96bcde66c64
Sebastian Pachon <111073223+SebsPachon@users.noreply.github.com>
2022-10-26 01:57:00 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -39,5 +39,5 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 -- SELECT strftime("%Y",c14) as "YEAR", COUNT(*)  FROM tbl1 WHERE YEAR == "2018" 
 -- SELECT strftime('%Y',c14), COUNT(*) FROM tbl1 WHERE strftime('%Y',c14) =='2018'
-SELECT strftime('%Y',c14), COUNT(c14) as CANT FROM tabla1 WHERE strftime('%Y',c14) =='2018'
+SELECT strftime('%Y',c14), COUNT(c14) as CANT FROM tbl1 WHERE strftime('%Y',c14) =='2018'
 
