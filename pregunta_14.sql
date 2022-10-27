cc9760d16334bd6ef516a195c06e97f33a89acd3
Sebastian Pachon <111073223+SebsPachon@users.noreply.github.com>
2022-10-27 13:36:43 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -42,5 +42,4 @@
 --  4  E  555.323077
 --
 --  >>> Escriba su codigo a partir de este punto <<<
-SELECT tbl1.K0, AVG(tbl2.c21) FROM tbl1, tbl2 WHERE tbl1.K1 =tbl2.K1 AND tbl1.c13 > 400 GROUP BY tbl1.K0
-
+SELECT tbl1.K0 AS [K0], AVG(tbl2.c21) AS [avg(c21)] FROM tbl1, tbl2 WHERE tbl1.K1 =tbl2.K1 AND tbl1.c13 > 400 GROUP BY [K0]
